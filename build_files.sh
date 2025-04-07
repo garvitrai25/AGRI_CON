6c8eafc5f9707e1d39b9d491e598f196c3c48d1d
garvitrai25 <garvitrai25@gmail.com>
2025-04-07 14:41:06 +0530

    Update build script to copy requirements file

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Build script for Vercel deployment
 
+echo "Setting up requirements.txt for Vercel deployment..."
+cp requirements-vercel.txt requirements.txt
+
 echo "Installing dependencies..."
-pip install -r requirements-vercel.txt
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
