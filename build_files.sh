3845b8d4fae4b49ae1bdad6c858b906a05a21403
garvitrai25 <garvitrai25@gmail.com>
2025-04-07 14:31:26 +0530

    Add simplified requirements for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 # Build script for Vercel deployment
 
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install -r requirements-vercel.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
